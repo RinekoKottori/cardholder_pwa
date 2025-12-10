b13ea76cc8d7ee1c3833e913d1e1eee057f5cf2c
Ekaterina Kuznetsova <kuznetsova.cat_37@mail.ru>
2025-12-10 14:59:57 +0300

    feat(objects): add Auth/Registration object definitions

---
+++ tests/front/_shared/credits.ts
@@ -0,0 +1,10 @@
+export const authorizedUser = {
+    email: "test@test.com",
+    username: "test",
+    password: "test",
+}
+
+export const loginUser = {
+    username: "Frodo",
+    password: "Beggins",
+}
