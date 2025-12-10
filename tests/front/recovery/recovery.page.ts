736392c568537ab8ed569561ecd76b87e8372bb9
Ekaterina Kuznetsova <kuznetsova.cat_37@mail.ru>
2025-12-10 14:53:00 +0300

    feat(pages): add new page objects for Recovery page

---
+++ tests/front/recovery/recovery.page.ts
@@ -0,0 +1,29 @@
+import { BasePage } from "../_shared/base.page";
+export enum ERecoveryPageLocators {
+  TITLE = "[class='password-recovery-request-header']",
+  USER_EMAIL_INPUT = "[id='user_email']",
+  SUBMIT_BUTTON = "[id='submit']",
+  REJECT_BUTTON = "[id='reject']",
+}
+
+/* path: /register */
+
+export class RecoveryPage extends BasePage {
+  readonly path = "/register";
+
+  override async open() {
+    await this.page.goto(this.url);
+  }
+
+  get title() {
+    return this.page.locator(ERecoveryPageLocators.TITLE);
+  }
+
+  get emailInput() {
+    return this.page.locator(ERecoveryPageLocators.USER_EMAIL_INPUT);
+  }
+
+  async fillEmail(email: string) {
+    await this.emailInput.fill(email);
+  }
+}
