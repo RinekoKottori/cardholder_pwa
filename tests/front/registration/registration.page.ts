8e268b0e616ab73bda5f82265abd3d1ec5973def
Ekaterina Kuznetsova <kuznetsova.cat_37@mail.ru>
2025-12-10 14:55:58 +0300

    feat(pages): add new page objects for Regestration page

---
+++ tests/front/registration/registration.page.ts
@@ -0,0 +1,56 @@
+import { BasePage } from "../_shared/base.page";
+export enum ERegistrationLocators {
+  TITLE = "[class='register-component-header']",
+  USER_NAME_INPUT = "[id='login_username']",
+  USER_EMAIL_INPUT = "[id='user_email']",
+  USER_PASSWORD_INPUT = "[id='password']",
+  USER_CONFIRM_PASSWORD_INPUT = "[id='confirm-password']",
+  REGISTER_BUTTON = "[data-testId='regisetry-button']",
+  EXIST_ACCOUNT_LINK = "[data-testId='already-have-account-link']",
+}
+
+/* path: /register */
+
+export class RegistrationrPage extends BasePage {
+  readonly path = "/register";
+
+  override async open() {
+    await this.page.goto(this.url);
+  }
+
+  get title() {
+    return this.page.locator(ERegistrationLocators.TITLE);
+  }
+
+  get emailInput() {
+    return this.page.locator(ERegistrationLocators.USER_EMAIL_INPUT);
+  }
+
+  get userNameInput() {
+    return this.page.locator(ERegistrationLocators.USER_NAME_INPUT);
+  }
+
+  get passwordInput() {
+    return this.page.locator(ERegistrationLocators.USER_PASSWORD_INPUT);
+  }
+
+  get confirmPasswordInput() {
+    return this.page.locator(ERegistrationLocators.USER_CONFIRM_PASSWORD_INPUT);
+  }
+
+  get registerButton() {
+    return this.page.locator(ERegistrationLocators.REGISTER_BUTTON);
+  }
+
+  get existAccountLink() {
+    return this.page.locator(ERegistrationLocators.EXIST_ACCOUNT_LINK);
+  }
+
+  async register(username: string, email: string, password: string) {
+    await this.emailInput.fill(email);
+    await this.userNameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.confirmPasswordInput.fill(password);
+    await this.registerButton.click();
+  }
+}
