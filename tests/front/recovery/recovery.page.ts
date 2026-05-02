a721c46a65f8f84ecf030a13e6f89b8503eeb244
Ekaterina Kuznetsova <kuznetsova.cat_37@mail.ru>
2026-05-02 14:59:39 +0300

    tests: fix locators, add new locator and page getters

--- tests/front/recovery/recovery.page.ts
+++ tests/front/recovery/recovery.page.ts
@@ -2,11 +2,10 @@ import { BasePage } from "../_shared/base.page";
 export enum ERecoveryPageLocators {
   TITLE = "[class='password-recovery-request-header']",
   USER_EMAIL_INPUT = "[id='user_email']",
-  SUBMIT_BUTTON = "[id='submit']",
-  REJECT_BUTTON = "[id='reject']",
+  SUBMIT_BUTTON = "[data-testid='password-recovery-request-button']",
+  REJECT_BUTTON = "[data-testid='password-recovery-cancel-link']",
+  GOT_CODE_BUTTON = "[routerLink='/password-recovery/submit']",
 }
-
-/* path: /register */
 
 export class RecoveryPage extends BasePage {
   readonly path = "/password-recovery/request";
@@ -23,7 +22,20 @@ export class RecoveryPage extends BasePage {
     return this.page.locator(ERecoveryPageLocators.USER_EMAIL_INPUT);
   }
 
-  async fillEmail(email: string) {
-    await this.emailInput.fill(email);
+  get gotCodeButton() {
+    return this.page.locator(ERecoveryPageLocators.GOT_CODE_BUTTON);
   }
+
+  get submitButton() {
+    return this.page.locator(ERecoveryPageLocators.SUBMIT_BUTTON);
+  }
+
+  get rejectButton() {
+    return this.page.locator(ERecoveryPageLocators.REJECT_BUTTON);
+  }
+
+  async recoverPassword(email: string) {
+    await this.emailInput.fill(email);
+    await this.submitButton.click();
+  };
 }
